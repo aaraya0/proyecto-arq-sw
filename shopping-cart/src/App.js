7d24b4584697f1310959f5ca1346a0390a465c35
AgostinaM <agosmorellato@gmail.com>
2022-05-24 15:58:36 -0300

    Login

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,23 +1,66 @@
-//import logo from './logo.svg';
-import './App.css';
-import List from "./components/List";
-import Create from "./components/Create";
-import {BrowserRouter as Router} from "react-router-dom";
-function App() {
-  return (
 
-    <Router>
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+async function getUserByID(id) {
+ return fetch('http://127.0.0.1:8090/user/' + id, {
+   method: 'GET',
+   headers: {
+     'Content-Type': 'application/json'
+   }
+ })
+   .then(data => data.json())
+}
+
+
+function App() {
+
+  const [userData, setUserData] = useState({});
+  const [isUser, setIsUSer] = useState(false);
   
-    <div className="container">
-      
-        <List ></List>
-      
-         <Create ></Create>
-      
- 
+  const handleSubmit = async event => {
+    //Prevent page reload
+    event.preventDefault();
+
+    // Obtenemos Textos
+    var {uid, upass}  = document.forms[0];
+
+    alert(upass.value);
+    // Find user login info
+    const user = await getUserByID(uid.value);
+    
+    setUserData(user);
+    setIsUSer(true);
+    
+  };
+
+  
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>User ID </label>
+          <input type="text" name="uid" required />
+          <label>User Pass </label>
+          <input type="password" name="upass" required />
+        </div>
+        <div className="button-container">
+          <input type="submit" value="Send Request" />
+        </div>
+      </form>
     </div>
-    </Router>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Login {userData.name}</div>
+          {isUser? <div>Usuario: {userData.name} </div> : renderForm}
+      </div>
+    </div>
   );
 }
 
 export default App;
+
