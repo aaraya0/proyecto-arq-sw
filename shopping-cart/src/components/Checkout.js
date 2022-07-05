de1c743cf1a0da425680045ba6b8d13408172145
aaraya <agusaraya131@gmail.com>
2022-07-05 15:12:55 -0300

    ordenes funcionando. falta control de stock y ordenes por usuario

--- shopping-cart/src/components/Checkout.js
+++ shopping-cart/src/components/Checkout.js
@@ -4,37 +4,55 @@ import Cookies from 'universal-cookie';
 import "./Checkout.css"
  
 const cookies = new Cookies();
-
+async function getProductById(id){
+  return fetch("http://localhost:8090/product/" + id).then(response => response.json())
+  }
 async function crearOrden(){
-    
-    let orderDetails=[]
-    let orderDetail={product_id:0, quantity:0}
-    let current= cookies.get('cart')
-    let items=[]
-    let a=current.split(',')
-    for (let i = 1; i < a.length; i++){
-    let item = a[i];
-    let prod= item.split('=')
-    let prodID=prod[0]
-    let cant= prod[1]
-    if (prodID!=0 && cant!="undefined" && cant>0){
-    orderDetail.product_id=parseInt(prodID)
-    alert (orderDetail.product_id)
-    orderDetail.quantity=parseInt(cant)
-    alert(orderDetail.quantity)
-orderDetails.push(orderDetail)
+  let details=[]
+  let current= cookies.get('cart')
+  let items=[]
+  let a=current.split(',')
+  for (let i = 0; i < a.length; i++){
+  let item = a[i];
+  let prod= item.split('=')
+  let prodID=prod[0]
+  let cant= prod[1]
+  if (prodID!=0 && prodID!="undefined" && cant!="undefined" && cant>0){
+  let product = {product_id:0, quantity:0}
+  product.quantity = cant;
+  product.product_id=prodID;
+items.push(product)
 }
-}
+  }
+items.forEach((item2) => {
+  let detail = {
+    product_id: Number(item2.product_id),
+    quantity: Number(item2.quantity)
+  }
+  details.push(detail)
+});
 
 
-return orderDetails
+
+return fetch("http://localhost:8090/order", {
+  method:"POST",
+  mode: 'no-cors',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    user_id: Number(cookies.get("user_id")),
+    orderDetail: details
+  })
+})
 }
 
 async function postOrden (){
     let user_Id = parseInt(cookies.get("user_id"))
    
-    var url = 'http://localhost:8090/order';
-var data = {"user_id":user_Id, "orderDetail":await(crearOrden())}
+    var url = 'http://localhost:8090/order'
+    let detail= await(crearOrden())
+var data = {"user_id":user_Id, "orderDetail": detail}
 
 fetch(url, {
   method: 'POST',
@@ -50,7 +68,7 @@ fetch(url, {
 
 function Checkout(){
     
-postOrden()
+crearOrden()
 
 
             return(
